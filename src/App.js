fb2a732619924b838d1c8d41e3f7f6ea64b5eec7
ArunBongi <arun.bongi19@gmail.com>
2024-12-01 15:06:34 -0800

    Update homepage

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/portfolio/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/experience" element={<Experience />} />
